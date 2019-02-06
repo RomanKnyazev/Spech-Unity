ada3051c1a1886db87dcd717bdd0e1f2d1e1c6cd
develop <develop@omega-r.com>
2019-02-06 17:17:15 +0300

    Add DictationManager and KeywordManager
    Add DictationManager and KeywordManager

---
+++ Unity-Spech/Assets/DictationManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+namespace Assets.Scenes
+{
+    public class DictationManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _hypotheses;
+
+        [SerializeField]
+        private Text _recognitions;
+
+        private DictationRecognizer _dictationRecognizer;
+
+        public DictationManager(Text recognitions, Text hypotheses)
+        {
+            _recognitions = recognitions;
+            _hypotheses = hypotheses;
+        }
+
+        private void Start()
+        {
+            _dictationRecognizer = new DictationRecognizer();
+
+            _dictationRecognizer.DictationResult += (text, confidence) =>
+            {
+                Debug.LogFormat("Dictation result: {0}", text);
+                _recognitions.text += text + "\n";
+            };
+
+            _dictationRecognizer.DictationHypothesis += (text) =>
+            {
+                Debug.LogFormat("Dictation hypothesis: {0}", text);
+                _hypotheses.text += text;
+            };
+
+            _dictationRecognizer.DictationComplete += (completionCause) =>
+            {
+                if (completionCause != DictationCompletionCause.Complete)
+                    Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+            };
+
+            _dictationRecognizer.DictationError += (error, hresult) =>
+            {
+                Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+            };
+
+            _dictationRecognizer.Start();
+        }
+    }
+}
---
+++ Unity-Spech/Assets/KeywordManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+namespace Assets.Scenes
+{
+    public class KeywordManager : MonoBehaviour
+    {
+        [SerializeField] private string[] _keywords;
+        private KeywordRecognizer _recognizer;
+
+        private void Start()
+        {
+            _recognizer = new KeywordRecognizer(_keywords);
+            _recognizer.OnPhraseRecognized += OnPhraseRecognized;
+            _recognizer.Start();
+        }
+
+        private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+        {
+            var builder = new StringBuilder();
+            builder.AppendFormat("{0} ({1}){2}", args.text, args.confidence, Environment.NewLine);
+            builder.AppendFormat("\tTimestamp: {0}{1}", args.phraseStartTime, Environment.NewLine);
+            builder.AppendFormat("\tDuration: {0} seconds{1}", args.phraseDuration.TotalSeconds, Environment.NewLine);
+            Debug.Log(builder.ToString());
+        }
+
+        private void OnApplicationQuit()
+        {
+            if (_recognizer == null || !_recognizer.IsRunning) return;
+
+            _recognizer.OnPhraseRecognized -= OnPhraseRecognized;
+            _recognizer.Stop();
+        }
+    }
+}
